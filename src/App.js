3204b826815e7db2d76d9b20b75383a03b131e46
Dave <bigmanrunning@gmail.com>
2023-04-19 13:38:20 +0000

    update

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   return (
     <div>
       <h1>Promises, promises</h1>
+      <h1>A Tutorial on Promises</h1>
       <PromiseComponent />
       <AnotherPromise />
       <FunctionPromise />
